8ab367221610259b54234368132ab5d889b22e49
hyunju@Julie-Kim <hyunju.kim520@gmail.com>
2019-06-11 18:36:02 +1000

    added navigation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
 
 import AuthPage from "./pages/Auth";
 import EventsPage from "./pages/Events";
 import BookingsPage from "./pages/Bookings";
+
+import MainNavigation from "./components/Navigation/MainNavigation";
 
 import "./App.css";
 
@@ -11,12 +13,17 @@ class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <Switch>
-          <Redirect from="/" to="/auth" exact />
-          <Route path="/auth" component={AuthPage} />
-          <Route path="/events" component={EventsPage} />
-          <Route path="/bookings" component={BookingsPage} />
-        </Switch>
+        <Fragment>
+          <MainNavigation />
+          <main className="main-content">
+            <Switch>
+              <Redirect from="/" to="/auth" exact />
+              <Route path="/auth" component={AuthPage} />
+              <Route path="/events" component={EventsPage} />
+              <Route path="/bookings" component={BookingsPage} />
+            </Switch>
+          </main>
+        </Fragment>
       </BrowserRouter>
     );
   }
