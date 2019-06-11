7c4e782bf73e75754c0a5ecb01cbcfba64ec8218
hyunju@Julie-Kim <hyunju.kim520@gmail.com>
2019-06-11 22:41:10 +1000

    Adding a Modal

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,8 +1,48 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
+
+import Modal from "../components/Modal/Modal";
+import Backdrop from "../components/Backdrop/Backdrop";
+
+import "./Events.css";
 
 class EventsPage extends Component {
+  state = {
+    createing: false
+  };
+
+  startCreateEventHandler = () => {
+    this.setState({ createing: true });
+  };
+
+  modalConfirmHandler = () => {};
+
+  modalCancelHandler = () => {
+    this.setState({ createing: false });
+  };
+
   render() {
-    return <h1>The Events Page</h1>;
+    return (
+      <Fragment>
+        {this.state.createing && <Backdrop />}
+        {this.state.createing && (
+          <Modal
+            title="Add Event"
+            canCancel
+            canConfirm
+            onCancel={this.modalCancelHandler}
+            onConfirm={this.modalConfirmHandler}
+          >
+            <p>Modal Content</p>
+          </Modal>
+        )}
+        <div className="events-control">
+          <p>Share your own Events!</p>
+          <button className="btn" onClick={this.startCreateEventHandler}>
+            Create Event
+          </button>
+        </div>
+      </Fragment>
+    );
   }
 }
 
