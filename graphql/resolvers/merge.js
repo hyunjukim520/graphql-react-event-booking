ddf7e7a2bddb2126fc213cbb3be12bbd0911c9b0
hyunju@Julie-Kim <hyunju.kim520@gmail.com>
2019-06-12 21:26:54 +1000

    #20 using Dataloader

--- graphql/resolvers/merge.js
+++ graphql/resolvers/merge.js
@@ -1,6 +1,50 @@
+const DataLoader = require("dataloader");
+
 const User = require("../../models/user");
 const Event = require("../../models/event");
 const { dateToString } = require("../../helpers/date");
+
+const eventLoader = new DataLoader(eventIds => {
+  return events(eventIds);
+});
+
+const userLoader = new DataLoader(userIds => {
+  return User.find({ _id: { $in: userIds } });
+});
+
+const events = async eventIds => {
+  try {
+    const events = await Event.find({ _id: { $in: eventIds } });
+    return events.map(event => {
+      return transformEvent(event);
+    });
+  } catch (err) {
+    throw err;
+  }
+};
+
+const singleEvent = async eventId => {
+  try {
+    const event = await eventLoader.load(eventId.toString());
+    return event;
+  } catch (err) {
+    throw err;
+  }
+};
+
+const user = async userId => {
+  try {
+    const user = await userLoader.load(userId.toString());
+    return {
+      ...user._doc,
+      password: null,
+      _id: user.id,
+      createdEvents: eventLoader.load.bind(this, user._doc.createdEvents)
+    };
+  } catch (err) {
+    throw err;
+  }
+};
 
 const transformEvent = event => {
   return {
@@ -22,43 +66,9 @@ const transformBooking = booking => {
   };
 };
 
-const events = async eventIds => {
-  try {
-    const events = await Event.find({ _id: { $in: eventIds } });
-    return events.map(event => {
-      return transformEvent(event);
-    });
-  } catch (err) {
-    throw err;
-  }
-};
-
-const singleEvent = async eventId => {
-  try {
-    const event = await Event.findById(eventId);
-    return transformEvent(event);
-  } catch (err) {
-    throw err;
-  }
-};
-
-const user = async userId => {
-  try {
-    const user = await User.findById(userId);
-
-    return {
-      ...user._doc,
-      password: null,
-      _id: user.id,
-      createdEvents: events.bind(this, user._doc.createdEvents)
-    };
-  } catch (err) {
-    throw err;
-  }
-};
+exports.transformEvent = transformEvent;
+exports.transformBooking = transformBooking;
 
 // exports.user = user;
 // exports.events = events;
 // exports.singleEvent = singleEvent;
-exports.transformEvent = transformEvent;
-exports.transformBooking = transformBooking;
